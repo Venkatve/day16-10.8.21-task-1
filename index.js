b4c726cef4d1cd2a13c851ab6b17b0ce05aa3721
Venkatve <venkat.ve.9@gmail.com>
2021-09-29 19:24:55 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+var mincount=1;
+var max;
+var tempt;
+
+for(i=0;i<arr1.length;i++){
+    for(j=0;j<arr1.length;i++){
+        if(arr1[i]==arr1[j])
+        max++;
+        
+        
+        if(mincount<max){
+            mincount==max;
+            tempt=(arr1[i])
+        }
+    }max=0;
+}
+console.log(temp+" "+mincount+"times");
